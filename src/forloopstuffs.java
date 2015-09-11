50321209e0e26393ee840069e4b7b07f596dbc39
eshaank <Guest@wts5.attlocal.net>
2015-09-10 19:55:35 -0700

    asfd
    asdfasdf

---
+++ src/forloopstuffs.java
@@ -0,0 +1,8 @@
+
+public class forloopstuffs {
+public static void main(String[] args) {
+	for (int i = 1; i < 777; i*=7) {
+		System.out.println(i);
+	}
+}
+}
---
+++ src/loopstuff.java
@@ -0,0 +1,14 @@
+
+public class loopstuff {
+public static void main(String[] args) {
+	for (int i = 1; i < 501; i++) {
+		
+		if (i % 2 == 0) {
+			System.out.println( i + "even");
+		}
+		if (i % 1 == 0) {
+			System.out.println( i + "odd");
+		}
+	}
+}
+}
