d3d456168b30f402f79be49e1c9222e90132a5a2
eshaank <Guest@wts8.wts.local>
2015-09-17 19:59:54 -0700

    vsdzs
    avsd

--- src/forloopstuffs.java
+++ src/forloopstuffs.java
@@ -1,8 +1,10 @@
-
 public class forloopstuffs {
-public static void main(String[] args) {
-	for (int i = 1; i < 777; i*=7) {
-		System.out.println(i);
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i+=1) {
+			for (int j = 0; j < 9; j+=3) {
+				System.out.println(i + "" + j );
+			}
+			System.out.println();
+		}
 	}
-}
 }
